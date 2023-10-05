5cb73eb207452f7b1f35f3ba6c903d2ec5196f71
TxOlmer <145970385+TxOlmer@users.noreply.github.com>
2023-10-05 13:05:05 +0200

    Added hitbox changing

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,17 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Transactions;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]       //needed for movement
 [RequireComponent(typeof(CircleCollider2D))] //needed for the rigidbody
+[RequireComponent(typeof(BoxCollider2D))]
 [RequireComponent(typeof(SpriteRenderer))]  //needed for visuals
-
 
 public class PlayerManager : MonoBehaviour
 {
     public BodyPartManager PartManager;
-    public Rigidbody2D RigidBody; 
+    public Rigidbody2D RigidBody;
+
+    private CircleCollider2D headCollider;
+    private BoxCollider2D bodyCollider;
+
     public bool IsGrounded { get; private set; }
     [SerializeField] private Vector2 _groundCheckSize;
     [SerializeField] private float _groundCheckDistance;
@@ -20,8 +25,15 @@ public class PlayerManager : MonoBehaviour
 
     void Start() {
         RigidBody = GetComponent<Rigidbody2D>();
-        PartManager.AddBodyPart(BodyPartType.RightLeg);
+        //PartManager.AddBodyPart(BodyPartType.RightLeg);
+        headCollider = GetComponent<CircleCollider2D>();
+        bodyCollider = GetComponent<BoxCollider2D>();
         _groundLayer = LayerMask.GetMask("Environment");
+        BodyPartManager.OnAbiltiesChanged += handleChangeColliders;
+        PartManager.ResetBodyParts();
+        PartManager.AddBodyPart(BodyPartType.LeftLeg);
+        //PartManager.AddBodyPart(BodyPartType.RightLeg);
+        Debug.Log("Started Player Manager");
     }
 
     void FixedUpdate()
@@ -45,6 +57,11 @@ public class PlayerManager : MonoBehaviour
     /// </summary>
     private void OnDrawGizmos() {
         Gizmos.DrawWireCube(transform.position - Vector3.up * _groundCheckDistance, _groundCheckSize);
+    }
+
+    private void OnDisable() {
+        Debug.Log("Disabled Player Manager");
+        BodyPartManager.OnAbiltiesChanged -= handleChangeColliders;
     }
 
     //public static event Action<PlayerState> OnPlayerStateChanged;
@@ -88,6 +105,48 @@ public class PlayerManager : MonoBehaviour
 
     //    }
     //}
+
+
+
+    // Behaviour
+    /*   Head: just roll back and forth
+     *   Head and leg: roll, or jump
+     *   Head and two legs: Run and jump
+     *   Head and arm: roll and push
+     *   Head and two arms: roll, push and pull
+     *   Head and leg
+     */
+
+
+    /*
+     Different behaviours
+        horizontal behaviour: head, two legs
+        push behaviour
+        pull behaviour
+
+     */
+
+
+
+    void handleChangeColliders() {
+        Debug.Log("abiltiesChanged");
+        if (PartManager.HasAbility[(int)PlayerAbilities.Roll]) {
+            headCollider.enabled = true;
+            bodyCollider.enabled = false;
+            RigidBody.freezeRotation = false;
+            _groundCheckSize = new Vector2(0.5f, 0.1f);
+            RigidBody.sharedMaterial.friction = 0.0f;
+        }
+        if (PartManager.HasAbility[(int)PlayerAbilities.Walk]) {
+            //if(bodyCollider.enabled) // handle setting the height of sprites of the player to correct position
+            headCollider.enabled = false;
+            bodyCollider.enabled = true;
+            transform.eulerAngles = Vector3.zero;
+            RigidBody.freezeRotation = true;
+            _groundCheckSize = new Vector2(0.95f, 0.1f);
+            RigidBody.sharedMaterial.friction = 0.1f;
+        }
+    }
 }
 
 [Serializable]
