9dc48ead87f2c4131a0950a9781d7127f91b8b5e
NickSot <nikola.s.sotirov@gmail.com>
2023-10-12 13:47:44 +0300

    make sound work

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [Header("AudioSource")]
-    [SerializeField] AudioSource musicSource;
-    [SerializeField] AudioSource SFXSource;
+    [SerializeField]
+    public AudioSource musicSource;
+
+    [SerializeField]
+    public AudioSource SFXSource;
 
     [Header("Audio Clip")]
     public AudioClip Background;
     public AudioClip Jump;
+
+    public static AudioManager Instance;
+
+    public AudioManager() { 
+        Instance = this;
+    }
+
     private void Start()
     {
         musicSource.clip = Background;
         musicSource.Play();
     }
+
     public void PlaySFX(AudioClip clip)
     {
         SFXSource.PlayOneShot(clip);
-
     }
 }
 
