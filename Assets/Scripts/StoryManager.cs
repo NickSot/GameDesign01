a4dd7bdd8f553931e8e813909d0c3be9c6537f0a
NickSot <nikola.s.sotirov@gmail.com>
2023-10-30 14:17:09 +0200

    show first element of the storyboard directly...

--- Assets/Scripts/StoryManager.cs
+++ Assets/Scripts/StoryManager.cs
@@ -19,6 +19,8 @@ public class StoryManager : MonoBehaviour
     void Start()
     {
         image = panel.GetComponent<Image>();
+        image.sprite = sprites[imageIndex];
+        imageIndex++;
     }
 
     // Update is called once per frame
